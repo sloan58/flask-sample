32cbfea3e8afef8fc002939d3098dd86e780b3ac
Marty Sloan <marty@karmatek.io>
2023-06-05 15:23:16 -0400

    Add simple version

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
-from routes.palo import palo_blueprint
-
+from flask import Flask, request
+from netmiko import ConnectHandler
+from netmiko.exceptions import SSHException
 
 app = Flask(__name__)
-app.register_blueprint(palo_blueprint, url_prefix='/palo')
+
+@app.post("/palo/test-fw-policy")
+def test_fw_policy():
+    params = request.json
+    try:
+        device = {
+            'device_type': 'cisco_ios',
+            'host': params['host'],
+            'username': params['user'],
+            'password': params['password'],
+        }
+        net_connect = ConnectHandler(**device)
+        output = net_connect.send_command('show ip int brief', use_textfsm=True)
+        return {'data': output}
+    except SSHException as e:
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
--- routes/palo.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from controllers.palo import test_fw_policy
-
-palo_blueprint = Blueprint('blueprint', __name__)
-
-palo_blueprint.route('/test-fw-policy', methods=['POST'])(test_fw_policy)
